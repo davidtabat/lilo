6d6a5635941ac312999987cca4f7586cabebf164
davidtabat <79899333+davidtabat@users.noreply.github.com>
2021-03-04 00:01:04 +0400

    Update form.php

--- form/form.php
+++ form/form.php
@@ -9,26 +9,19 @@ try {
     $fname = $_POST['fname'];
     $lname = $_POST['lname'];
     $email = $_POST['email'];
+    if(isset($email)) echo "Form submitted";
+    else { echo "error";}
     $to = 'test@developers-alliance.com';
     $subject = 'Form data';
-    $message = 'First Name: ' . $fname . 'Last Name: ' . $lname . 'email: . $email' ;
-    $headers = "From: hello@email-address.com\r\n";
+    $message = 'First Name: ' . $fname . ' Last Name: ' . $lname . ' e-mail: ' . $email  ;
+    $headers = "From: your@email-address.com\r\n";
 
-   if(mail($to, $subject, $message, $headers)){
-           $response = "Form submitted";
-   }
-        else {
-                $response = "Failed to submit";        
-   }
+    mail($to, $subject, $message, $headers);
+
 
     $pdoQuery = "INSERT INTO `users`(`fname`, `lname`, `email`) VALUES (:fname,:lname,:email)";
     
     $pdoResult = $pdoConnect->prepare($pdoQuery);
     
-    $pdoExec = $pdoResult->execute(array(":fname"=>$fname,":lname"=>$lname,":email"=>$email));
-
-    
-    echo $response;
-
-
+    $pdoExec = $pdoResult->execute(array(":fname"=>$fname,":lname"=>$lname,":email"=>$email)); 
 ?>
