7ba68250e0313de9cd40f0034c4b1e874650147e
davidtabat <79899333+davidtabat@users.noreply.github.com>
2021-03-15 18:49:39 +0400

    Update form.php

--- form/form.php
+++ form/form.php
@@ -9,19 +9,20 @@ try {
     $fname = $_POST['fname'];
     $lname = $_POST['lname'];
     $email = $_POST['email'];
-    if(isset($email)) echo "Form submitted";
-    else { echo "error";}
+    
+  
     $to = 'test@developers-alliance.com';
-    $subject = 'Form data';
-    $message = 'First Name: ' . $fname . ' Last Name: ' . $lname . ' e-mail: ' . $email  ;
+    $subject = 'This is a Mailhog test';
+    $message = 'First Name: ' . $fname . 'Last Name: ' . $lname . 'e-mail: ' . $email  ;
     $headers = "From: your@email-address.com\r\n";
 
     mail($to, $subject, $message, $headers);
 
 
     $pdoQuery = "INSERT INTO `users`(`fname`, `lname`, `email`) VALUES (:fname,:lname,:email)";
-    
     $pdoResult = $pdoConnect->prepare($pdoQuery);
     
     $pdoExec = $pdoResult->execute(array(":fname"=>$fname,":lname"=>$lname,":email"=>$email)); 
+    if(isset($email)) echo "Form submitted";
+    else { echo "error";}
 ?>
